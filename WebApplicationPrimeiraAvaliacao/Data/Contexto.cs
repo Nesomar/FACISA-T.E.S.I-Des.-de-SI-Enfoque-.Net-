7a655e888bd1c2c1c95993bc36542ce572068256
Nesomar <nesomar.ramalho@gmail.com>
2017-10-02 21:41:29 -0300

    Atividade Primeira Nota

---
+++ WebApplicationPrimeiraAvaliacao/Data/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using WebApplicationPrimeiraAvaliacao.Models;
+
+namespace WebApplicationPrimeiraAvaliacao.Data
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Cliente> Cliente { get; set; }
+    }
+}
